eeb016d6ba1407b35e2d83807e94bb78b9c0f097
Menescal <daniel.menescal@snowmanlabs.com>
2024-08-28 11:20:19 -0300

    feat: create service

--- README.md
+++ README.md
@@ -21,3 +21,26 @@ $ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 protoc --go_out=. --go-grpc_out=. proto/course_category.proto
 </pre>
 The **pb** directory should be created.
+
+Next run mod to updated libreries
+<pre>
+go mod tidy
+</pre>
+
+Install gRPC Evans 
+[https://evans.syfm.me/about](https://evans.syfm.me/about)
+
+Run evans with the command bellow
+<pre>
+evans -r repl
+</pre>
+
+In Evans cmd you may need to set the current package
+<pre>
+package pb
+</pre>
+
+In Evans cmd call the service to execute methotds
+<pre>
+service CategoryService
+</pre>
--- cmd/grpc/main.go
+++ cmd/grpc/main.go
@@ -1,6 +1,41 @@
 package main
 
 import (
+	"database/sql"
+	"grpcgo/internal/database"
+	"grpcgo/internal/pb"
+	"grpcgo/internal/service"
+	"net"
+
 	_ "github.com/google/uuid"
 	_ "github.com/mattn/go-sqlite3"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+
+	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS categories (id TEXT, name TEXT, description TEXT)"); err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	categoryDB := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDB)
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+}
